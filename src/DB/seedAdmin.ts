3e947be6805c86cf661f9141325e5676d7d8edaf
adnanmahmud0 <adnan99mahmud@gmail.com>
2025-07-31 00:32:13 +0600

    some setup is doen

---
+++ src/DB/seedAdmin.ts
@@ -0,0 +1,24 @@
+import { User } from '../app/modules/user/user.model';
+import config from '../config';
+import { USER_ROLES } from '../enums/user';
+import { logger } from '../shared/looger';
+
+
+const payload = {
+  name: 'Administrator',
+  email: config.super_admin.email,
+  role: USER_ROLES.SUPER_ADMIN,
+  password: config.super_admin.password,
+  verified: true,
+};
+
+export const seedSuperAdmin = async () => {
+  const isExistSuperAdmin = await User.findOne({
+    email: config.super_admin.email,
+    role: USER_ROLES.SUPER_ADMIN,
+  });
+  if (!isExistSuperAdmin) {
+    await User.create(payload);
+    logger.info('✨ Super Admin account has been successfully created!');
+  }
+};
