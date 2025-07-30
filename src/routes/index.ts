a01bb99ce5bf70549d3517e31e35b2636149431a
adnanmahmud0 <adnan99mahmud@gmail.com>
2025-07-31 01:29:49 +0600

    auth and user module created

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { AuthRoutes } from '../app/modules/auth/auth.route';
 import { UserRoutes } from '../app/modules/user/user.route';
+
 const router = express.Router();
 
 const apiRoutes = [
